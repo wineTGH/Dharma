610c8f04073f85b7665526c9f57d2bdef84d68e6
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2023-04-08 23:35:43 +1000

    cdrom info

--- client/main.py
+++ client/main.py
@@ -1,7 +1,12 @@
 # Python file template
 
 # By wineT (Anatoly Bogomolov)
+import wmi
 
+def main():
+	c = wmi.WMI()
+	for cdrom in c.Win32_CDROMDrive():
+		print(cdrom.Drive, cdrom.MediaLoader)
 
 if __name__ == "__main__":
-	...
+	main()
